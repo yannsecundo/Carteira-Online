1a22e94a20956271b7f3c2c56b0234e0266505c9
Yann Secundo <yannsecundo34@gmail.com>
2022-10-19 09:48:48 -0400

    analisando problemas com rota

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 import Routes from './Rotas/rotas';
-import Login from './pages/Login';
-import Wallet from './pages/Wallet';
 
 function App() {
   return <Routes />;
