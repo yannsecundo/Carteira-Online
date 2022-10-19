71b7a949bb1fa37076505bf4b7b623d888e5ccba
Yann Secundo <yannsecundo34@gmail.com>
2022-10-19 09:12:05 -0400

    testando uma nova abordagem de rota

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/carteira" component={ Wallet } />
-    </Switch>);
+  <Switch>
+    <Route exact path="/" component={ Login } />
+    <Route exact path="/carteira" component={ Wallet } />
+  </Switch>
 }
 
 export default App;
