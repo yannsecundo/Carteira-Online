4ca979dde64900d64ab5e7d69ffbe1f8727e84ec
Yann Secundo <yannsecundo34@gmail.com>
2022-10-19 12:27:36 -0400

    resolvendo problemas de rotas

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 function App() {
   return (
     <Switch>
-      <Route path="/" component={ Login } />
-      <Route path="/carteira" component={ Wallet } />
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/carteira" component={ Wallet } />
     </Switch>
   );
 }
